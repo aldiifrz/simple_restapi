f402fb96e7b057a6f4847e8d52165811259f578e
Aldi <aldi.fariz@gmail.com>
2024-10-05 16:43:09 +0700

    debugging and added id increment + DeleteProduct

--- handler/product.go
+++ handler/product.go
@@ -2,9 +2,11 @@ package handler
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 	"simple_restapi/dto"
 	"simple_restapi/entity"
+	"strconv"
 )
 
 var ProductItems = []entity.Product{
@@ -15,47 +17,107 @@ var ProductItems = []entity.Product{
 		Price: 150000,
 	},
 	{
-		ID:    1,
+		ID:    2, //  Changed ID from 1 to 2
 		Name:  "Joger",
 		Qty:   120,
 		Price: 120000,
 	},
 	{
-		ID:    1,
+		ID:    3, //  Changed ID from 1 to 3
 		Name:  "Jeans",
 		Qty:   150,
 		Price: 100000,
 	},
 }
 
-func GetProduct(w http.ResponseWriter, r *http.Request) {
+func getNextID() int {
+	maxID := 0
+	for _, item := range ProductItems {
+		if item.ID > maxID {
+			maxID = item.ID
+		}
+	}
+	return maxID + 1
+}
 
+func GetProduct(w http.ResponseWriter, r *http.Request) {
 	resData := dto.ResponseModels{
 		ResponseCode:    http.StatusOK,
 		ResponseMessage: "Success",
 		Data:            ProductItems,
 	}
-	w.Header().Set("content-type", "application/json")
+
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK) // 200 OK
 	json.NewEncoder(w).Encode(resData)
 
 }
+
 func AddProduct(w http.ResponseWriter, r *http.Request) {
 	var ProductItem entity.Product
+
+	// Decode the incoming JSON request
 	err := json.NewDecoder(r.Body).Decode(&ProductItem)
 	if err != nil {
+		// Log the exact error to understand the problem
+		log.Printf("Error decoding JSON : %v", err)
 		http.Error(w, "invalid request", http.StatusBadRequest) //400
+		return
 	}
 
-	ProductItem.ID = len(ProductItems) + 1
+	// Assign a unique ID to the new product
+	ProductItem.ID = getNextID()
+
+	// Append the new product to the list of products
 	ProductItems = append(ProductItems, ProductItem)
+
+	// Prepare the response data
 	resData := dto.ResponseModels{
-		ResponseCode:    http.StatusOK,
-		ResponseMessage: "Success",
-		Data:            ProductItems,
+		ResponseCode:    http.StatusCreated, // Use 201 Created for a POST request
+		ResponseMessage: "Product added successfully",
+		Data:            ProductItems, // Include the updated list of products
 	}
-	w.Header().Set("content-type", "application/json")
-	w.WriteHeader(http.StatusOK)
+
+	// Set the response headers and status code
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	// Encode the response as JSON and send it back
 	json.NewEncoder(w).Encode(resData)
 
+}
+
+func DeleteProduct(w http.ResponseWriter, r *http.Request) {
+	// Get the product ID from the URL query parameter
+	productIDStr := r.URL.Query().Get("id")
+
+	// Convert the product ID from string to integer
+	productID, err := strconv.Atoi(productIDStr)
+	if err != nil || productID <= 0 {
+		http.Error(w, "invalid product ID", http.StatusBadRequest)
+		return
+	}
+
+	// Find and remove the product with the given ID
+	for index, item := range ProductItems {
+		if item.ID == productID {
+			// Remove the product by slicing the array
+			ProductItems = append(ProductItems[:index], ProductItems[index+1:]...)
+
+			// Prepare success response
+			resData := dto.ResponseModels{
+				ResponseCode:    http.StatusOK,
+				ResponseMessage: "Product deleted successfully",
+				Data:            ProductItems,
+			}
+
+			w.Header().Set("Context-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(resData)
+			return
+		}
+
+		// If the product with the given ID is not found, return an error
+		http.Error(w, "product not found", http.StatusNotFound)
+	}
 }
--- main.go
+++ main.go
@@ -6,9 +6,21 @@ import (
 	"simple_restapi/handler"
 )
 
+func productHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		handler.GetProduct(w, r)
+	case "POST":
+		handler.AddProduct(w, r)
+	case "DELETE":
+		handler.DeleteProduct(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
 func main() {
-	http.HandleFunc("/product", handler.GetProduct)
-	http.HandleFunc("/product", handler.AddProduct)
+	http.HandleFunc("/product", productHandler)
 
 	fmt.Println("Server is running on port 8080")
 	err := http.ListenAndServe(":8080", nil)
