afa61a969a339a44ad76c0ecaa0f22a4d68e0e24
Aldi <aldi.fariz@gmail.com>
2024-10-24 00:16:15 +0700

    [change] data & price for more flexibility

--- internal/product/dto/response.go
+++ internal/product/dto/response.go
@@ -1,9 +1,7 @@
 package dto
 
-import "simple_restapi/internal/product/entity"
-
 type ResponseModels struct {
-	ResponseCode    int              `json:"response_code"`
-	ResponseMessage string           `json:"response_message"`
-	Data            []entity.Product `json:"data"`
+	ResponseCode    int         `json:"response_code"`
+	ResponseMessage string      `json:"response_message"`
+	Data            interface{} `json:"data"` // Use interface{} to support various types of data
 }
--- internal/product/entity/product.go
+++ internal/product/entity/product.go
@@ -1,8 +1,8 @@
 package entity
 
 type Product struct {
-	ID    int    `json:"id"`
-	Name  string `json:"name"`
-	Qty   int    `json:"qty"`
-	Price int    `json:"price"`
+	ID    int     `json:"id"`
+	Name  string  `json:"name"`
+	Qty   int     `json:"qty"`
+	Price float64 `json:"price"`
 }
